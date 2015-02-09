2b87477a6c202e2ff0f86ac6b710d0503a27665e
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2015-02-09 20:23:44 -0300

    Agrega comentarios doxygen

--- nrf/inc/nrf.h
+++ nrf/inc/nrf.h
@@ -14,24 +14,24 @@ typedef enum {
 	NRF_TX, NRF_RX
 } nrf_mode_t;
 
-/**
+/** @brief
  * Inicializa el dispositivo  nrf24l01+
  */
 void nrf_init(nrf_mode_t mode, uint8_t address[5]);
 
-/**
+/** @brief
  * Envía un byte a la dirección que se le configuró al dispositivo.
  */
 void nrf_send(uint8_t c);
 
-/**
+/** @brief
  * Devuelve un word con:
  *      LSB: byte recibido por el dispositivo
  *      MSB: código de error en el MSB.
  */
 uint8_t nrf_receive();
 
-/**
+/** @brief
  * Devuelve la cantidad de bytes disponibles para lectura.
  */
 uint8_t nrf_available();
