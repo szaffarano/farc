83c9d25007d9a828be30321e34dd6be71e6b573f
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2015-03-24 12:14:10 -0300

    Se genera librería de debounce.  Falta hacer agnóstica del uc

---
+++ nrf-ng/inc/debounce/debounce.h
@@ -0,0 +1,13 @@
+/*
+ * debounce.h
+ *
+ *  Created on: 24/03/2015
+ *      Author: sebas
+ */
+
+#ifndef DEBOUNCE_H_
+#define DEBOUNCE_H_
+
+#define DEBOUNCE_DELAY 20
+
+#endif /* DEBOUNCE_H_ */
---
+++ nrf-ng/src/debounce/debounce.c
@@ -0,0 +1,59 @@
+/*
+ * debounce.c
+ *
+ *  Created on: 24/03/2015
+ *      Author: sebas
+ */
+
+#include <debounce/debounce.h>
+#include <nrf-ng.h>
+
+typedef enum {
+	WAITING_PUSH, NOISE_PUSH, PUSHING, NOISE_RELEASE
+} debounce_state;
+
+debounce_event farc_debounce(void) {
+	static debounce_state state = WAITING_PUSH;
+	static int8_t ticks = DEBOUNCE_DELAY;
+
+	debounce_event event = RELEASED;
+
+	if (ticks-- <= 0) {
+		switch (state) {
+		case WAITING_PUSH:
+			if (bit_is_clear(SW1_PIN, SW1)) {
+				state = NOISE_PUSH;
+				ticks = DEBOUNCE_DELAY;
+			}
+			break;
+		case NOISE_PUSH:
+			event = FELL;
+			if (bit_is_clear(SW1_PIN, SW1)) {
+				state = PUSHING;
+				event = FELL;
+			} else {
+				state = WAITING_PUSH;
+			}
+			break;
+		case PUSHING:
+			if (bit_is_set(SW1_PIN, SW1)) {
+				ticks = DEBOUNCE_DELAY;
+				state = NOISE_RELEASE;
+			} else {
+				event = PUSHED;
+			}
+			break;
+		case NOISE_RELEASE:
+			if (bit_is_clear(SW1_PIN, SW1)) {
+				state = PUSHING;
+			} else {
+				// liberaron el boton
+				state = WAITING_PUSH;
+
+				event = ROSE;
+			}
+			break;
+		}
+	}
+	return event;
+}
